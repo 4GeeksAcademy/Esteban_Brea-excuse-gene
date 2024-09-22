69dfb42c5f7885a41178a424c558e0bd6ad9d8b7
Esteban Brea <83199483+estebanbrea@users.noreply.github.com>
2024-09-22 03:10:37 +0000

    Proyecto-Esteban_Brea-Excuse-Gen

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let randomElement = array => {
+      let numArray = Math.floor(Math.random() * array.length);
+      return array[numArray];
+    };
+
+    let randomWho = randomElement(who);
+    let randomAction = randomElement(action);
+    let randomWhat = randomElement(what);
+    let randomWhen = randomElement(when);
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+  }
+
+  let excuseElement = document.querySelector("#excuse");
+  excuseElement.innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
 };
